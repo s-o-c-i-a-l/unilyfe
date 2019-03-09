f9924255a8613a09a16333c932d48b5bc4cc6760
Emily <haw.emily@gmail.com>
2019-03-09 12:55:11 +0000

    cleaned up code

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
--- src/main/java/Society.java
+++ src/main/java/Society.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 public class Society implements Categorisable {
