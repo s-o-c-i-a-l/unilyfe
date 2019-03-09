44d0629d11ce501cb5f949c29d4053595eee2453
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2019-03-09 15:17:46 +0000

    Improve dataflow for new society component

--- src/NewSociety/NewSociety.js
+++ src/NewSociety/NewSociety.js
@@ -1,7 +1,26 @@
-<link rel="stylesheet" href="./new-soc.css">
-<div id="main-div" class="centered">
-    <div id="cover-photo-div" class="centered">
-        <img src="https://mtsoc.co.uk/img/revue2014.jpg" id="cover-photo"></img>
-    </div>
-    <h2>Musical Theatre Society</h2>
-</div>
+import React, { Component } from 'react';
+import './NewSociety.css';
+import { join } from 'upath';
+
+class NewSociety extends Component {
+    constructor(societyName) {
+        super();
+        this.societyName = "Musical Theatre Society";
+    }
+    joinSociety() {
+        alert(`Joined ${this.societyName}!`);
+    }
+    render() {
+        return (
+            <div id="main-div" className="centered">
+                <div id="cover-photo-div" className="centered">
+                    <img src="https://mtsoc.co.uk/img/revue2014.jpg" id="cover-photo"></img>
+                </div>
+                <h2>Musical Theatre Society</h2>
+                <button onClick={() => this.joinSociety(this.societyName)}>Join</button>
+            </div>
+        );
+    }
+}
+
+export default NewSociety;
