70504467ef59fe46b6c0b948d7fc2466c965b05e
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2019-03-09 14:16:40 +0000

    Add folder for NewSociety page

---
+++ src/NewSociety/NewSociety.js
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="./new-soc.css">
+<div id="main-div" class="centered">
+    <div id="cover-photo-div" class="centered">
+        <img src="https://mtsoc.co.uk/img/revue2014.jpg" id="cover-photo"></img>
+    </div>
+    <h2>Musical Theatre Society</h2>
+</div>
