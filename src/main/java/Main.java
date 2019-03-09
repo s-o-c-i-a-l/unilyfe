c7687e8bb1c3a2bf8a93972385a6ba3366350f0a
Patrick Wu <magetron1@gmail.com>
2019-03-09 15:14:12 +0000

    add HTTP request handler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
 public class Main {
+    class AppHandler implements HttpHandler {
+        public void handle (HttpExchange exchange) throws IOException {
+            String requestMethod = exchange.getRequestMethod();
+        }
+    }
+
+
+    private void init () {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new AppHandler());
+        server.setExecutor(Executors.newCachedThreadPool());
+        server.start();
+    }
 
     public static void main(String[] args) {
-
+        new Main().init();
     }
 }
